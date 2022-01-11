440bcccb0226e52c8551bfc63ec43bd9a47bf202
VOID <kouichi.hirachi@gmail.com>
2022-01-11 18:00:06 +0900

    fix: TSを導入,フォルダ整理

--- serverless/dynamodb/docker/createTable.sh
+++
@@ -1,3 +0,0 @@
-file="dynamodb.yml"
-ruby -e 'require "yaml";require "json";y=YAML.load ARGF.read;puts JSON.pretty_generate y["Resources"]["Table"]["Properties"]' ../templates/$file /tmp/$file
-aws dynamodb create-table --endpoint-url http://localhost:8000 --cli-input-json file:///tmp/$file
