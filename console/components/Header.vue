f1b126d46ef167ec545be86b9d73dec1424bb7b3
VOID <kouichi.hirachi@gmail.com>
2022-03-03 21:30:31 +0900

    fix: ナビゲーションの表記を管理画面に変更

--- console/components/Header.vue
+++ console/components/Header.vue
@@ -9,7 +9,7 @@
         <b-navbar-nav class="ml-auto">
           <b-nav-item v-on:click="login" v-if="!this.$store.state.auth.loggedIn">ログイン</b-nav-item>
           <b-nav-item-dropdown right :text="this.$store.state.auth.user.name" v-else>
-            <b-dropdown-item to="/api">ユーザー設定</b-dropdown-item>
+            <b-dropdown-item to="/api">管理画面</b-dropdown-item>
             <b-dropdown-item v-on:click="logout">ログアウト</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
